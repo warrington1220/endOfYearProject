befb25a089a5eed7588802201cb061d179ad535a
warrington1220 <waarrington@gmail.com>
2020-12-22 21:44:56 -0800

    App Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'endOfYearProject';
+
+  myForm: FormGroup;
+  baseWriteURL = "https://braxos-tools.azurewebsites.net/api/NewLight?code=QaTrEl6lGe6/sRc/yuzM/QngkZ/XvOp2ni0ecvLKMtf7CKRhph8IMQ=="
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      red: '',
+      green: '',
+      blue: '',
+      x: '',
+      y: ''
+    })
+    //this.myForm.valueChanges.subscribe(console.log)
+  }
+
+  pushLight() {
+    var returnData = this.myForm.value
+    console.log(this.myForm.value)
+    this.http
+      .post<any>(this.baseWriteURL, { schedule: returnData})
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
