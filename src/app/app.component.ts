b953d8c59acf0ed5c36a14de260186c4b8eb9c9d
warrington1220 <waarrington@gmail.com>
2020-12-23 08:44:12 -0800

    Small UI update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
